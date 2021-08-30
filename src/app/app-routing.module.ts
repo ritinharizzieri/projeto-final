ad04a2d07d7470bea423be8ecb21bb4f2d5728cc
ritinharizzieri <ritarizzieri@gmail.com>
2021-08-30 14:45:09 -0300

    feat: criando e integrando o modulo Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardRoutes } from './dashboard/dashboard-routing.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  ...DashboardRoutes,
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
